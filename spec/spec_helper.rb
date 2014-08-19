2f17de4a3462c4f418256314e65a78ce41ca3b9a
mpikula <mariusz.pikula@gmail.com>
2014-08-19 17:57:50 +0200

    cassetes removal

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,18 +2,19 @@
 
 require 'rubygems'
 require 'rspec'
-require 'vcr'
+#require 'vcr'
 
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'pinterest_reporter'
 
-VCR.configure do |c|
-  c.configure_rspec_metadata!
-  c.cassette_library_dir     = 'spec/cassettes'
-  c.hook_into                :webmock
-  c.default_cassette_options = { :record => :new_episodes }
-end
+# VCR.configure do |c|
+#   c.configure_rspec_metadata!
+#   c.cassette_library_dir     = 'spec/cassettes'
+#   c.hook_into                :webmock
+#   c.default_cassette_options = { :record => :new_episodes }
+#   c.allow_http_connections_when_no_cassette = true
+# end
 
 RSpec.configure do |c|
 end
